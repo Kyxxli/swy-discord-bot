88e880d25506f3f53c12af1d1c0f166cb8481c56
Kyxxli <kyxx.li32@gmail.com>
2025-01-05 22:44:47 -0700

    :sparkle: Utilidad para añadir datos extra a los comandos requerido por &help (nyxx por defecto no permite esto, o no hasta donde yo sé)

---
+++ bin/utils/get_cmd_extra_data.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+import 'package:path/path.dart' as path;
+import 'package:yaml/yaml.dart';
+// import '../types/structs.dart';
+
+Future<dynamic> getCommandExtraData(String cmd) async {
+  String commandPath = path.join(Directory.current.path, 'bin', 'commands', '${cmd}_command.yml');
+  File commandData = File(commandPath);
+  String fileContent = await commandData.readAsString();
+
+  return loadYaml(fileContent);
+}
+
+String examplesFromArray(YamlList arr) {
+  String finalStr = '';
+
+  for (var i = 0;i < arr.length;i++) {
+    finalStr += '`${arr[i]}`\n';
+  }
+
+  return finalStr;
+}
