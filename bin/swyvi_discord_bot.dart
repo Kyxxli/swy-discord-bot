295accdb67add6d6d9ec4d30c26a1493ace484b6
Kyxxli <kyxx.li32@gmail.com>
2025-01-06 19:43:14 -0700

    :art: Funciones y variables innecesarias eliminadas + añadido el plugin ignoreExceptions

--- bin/swyvi_discord_bot.dart
+++ bin/swyvi_discord_bot.dart
@@ -1,15 +1,11 @@
-// import 'dart:io';
 import 'package:nyxx/nyxx.dart';
 import 'package:nyxx_commands/nyxx_commands.dart';
 import 'package:dotenv/dotenv.dart';
-import 'package:logger/logger.dart' as l;
-
 // Load all commands
 import 'package:swyvi_discord_bot/swyvi_discord_bot.dart';
 
 void main() async {
   final environ = DotEnv(includePlatformEnvironment: true)..load();
-  final logger = l.Logger();
 
   CommandsPlugin commands = CommandsPlugin(
     prefix: (MessageCreateEvent ev) {
@@ -19,24 +15,21 @@ void main() async {
     guild: Snowflake.parse(environ['SWY_TEST_GUILD'] as String),
 
     options: CommandsOptions(
-      logErrors: true
+      logErrors: true,
     )
   );
 
-  final swyClient = await Nyxx.connectGateway(
+  await Nyxx.connectGateway(
     environ['SWY_AUTH_SECRET'] as String,
     GatewayIntents.all,
     options: GatewayClientOptions(
-      plugins: [commands, logging]
+      plugins: [
+        commands,
+        logging,
+        ignoreExceptions,
+      ]
     )
   );
-
-  final swyUser = await swyClient.users.fetchCurrentUser();
-
-  swyClient.onReady.listen((ReadyEvent e) {
-    logger.i('Swy~vi is connected to the Discord gateway!');
-    logger.d('Connected as ${swyUser.username}');
-  });
 
   // Add all commands
   commands.addCommand(helpCommand);
