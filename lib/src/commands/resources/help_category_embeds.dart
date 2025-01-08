996fd5ec480efca9d533387d29d5154b5d08ba7c
Kyxxli <kyxx.li32@gmail.com>
2025-01-07 19:07:01 -0700

    :recycle: Reorganizado de comandos

--- lib/src/commands/resources/help_category_embeds.dart
+++
@@ -1,9 +0,0 @@
-import 'package:nyxx/nyxx.dart';
-
-EmbedBuilder infoEmbed = EmbedBuilder(
-  title: '▶️ Categoría: Información',
-  description:
-  'Estos son los comandos disponibles en la categoría '
-  'de Información.\n'
-  '```\nhelp, info, invite, support```',
-);
