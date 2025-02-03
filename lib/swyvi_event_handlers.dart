8f48333b13123b879da119b9ea685121d010fda2
Kyxxli <kyxx.li32@gmail.com>
2025-02-02 17:28:19 -0700

    :sparkles: Se añadió un sistema para almacenar datos de un servidor.
    Esto puede ser útil por si en un futuro se reintroducen los prefijos, o para
    almacenar datos como si el servidor aceptó los terminos y condiciones,
    preferencias, etc.

--- lib/swyvi_event_handlers.dart
+++ lib/swyvi_event_handlers.dart
@@ -14,12 +14,83 @@ class EventHandlers {
   void listenBot(NyxxGateway bot) {
     bot.onReady.listen(EventHandlers.onReady);
     bot.onMessageDelete.listen(EventHandlers.onMessageDelete);
+    bot.onGuildCreate.listen(EventHandlers.onGuildCreate);
+    bot.onGuildDelete.listen(EventHandlers.onGuildDelete);
   }
 
   // Bot events
   static void onReady(ReadyEvent ev) {
+    print('[>>>] [EVENT::READY] Connected as ${ev.user.username}');
+
     if (Platform.environment['SWY_TEMP'] == null) {
       print('[@] SWY_TEMP env is not defined. Be aware of further errors.');
+    }
+  }
+
+  static void onGuildCreate(UnavailableGuildCreateEvent ev) async {
+    final guild = await ev.guild.fetch();
+
+    print('[>>>] [EVENT::GUILD_CREATE] Joined guild ${guild.name} with id ${guild.id}\u0007');
+
+    final String swyDataPath = Platform.environment['SWY_DATA']!;
+    final Directory swyDataDir = Directory(swyDataPath);
+
+    if (!swyDataDir.existsSync()) {
+      swyDataDir.createSync(recursive: true);
+    }
+
+    final List<FileSystemEntity> swyDataEntities = swyDataDir.listSync();
+    List<String> swyDataChildren = [];
+
+    // Obtener el nombre de cada archivo
+    for (final child in swyDataEntities) {
+      swyDataChildren.add(child.uri.pathSegments.last);
+    }
+
+    // Crear un archivo para cada servidor si no existe
+    for (final g in ev.gateway.client.guilds.cache.keys) {
+      if (!swyDataChildren.contains('$g.guild.json')) {
+        final File dataFile = File(path.join(swyDataPath, '$g.guild.json'))..createSync();
+        dataFile.writeAsStringSync('{ "join_date": "${g.timestamp.millisecondsSinceEpoch}" }');
+      }
+    }
+
+    final File dataFile = File(path.join(swyDataPath, '${ev.guild.id}.guild.json'))..createSync();
+    final String dataCnt = dataFile.readAsStringSync();
+    final Map<String, dynamic> dataJson = jsonDecode(dataCnt);
+
+    dataJson['guild_name'] = guild.name;
+    dataJson['swy_terms_compliant'] = false;
+    dataJson['swy_prefix'] = '&';
+
+    dataFile.writeAsStringSync(jsonEncode(dataJson));
+  }
+
+  static void onGuildDelete(GuildDeleteEvent ev) async {
+    final guild = await ev.guild.fetch();
+
+    print('[>>>] [EVENT::GUILD_DELETE] Left guild ${guild.name} with id ${guild.id}');
+
+    final String swyDataPath = Platform.environment['SWY_DATA']!;
+    final Directory swyDataDir = Directory(swyDataPath);
+
+    if (!swyDataDir.existsSync()) {
+      swyDataDir.createSync(recursive: true);
+    }
+
+    final List<FileSystemEntity> swyDataEntities = swyDataDir.listSync();
+    List<String> swyDataChildren = [];
+
+    // Obtener el nombre de cada archivo
+    for (final child in swyDataEntities) {
+      swyDataChildren.add(child.uri.pathSegments.last);
+    }
+
+    // Crear un archivo para cada servidor si no existe
+    for (final g in ev.gateway.client.guilds.cache.keys) {
+      if (swyDataChildren.contains('$g.guild.json')) {
+        File(path.join(swyDataPath, '$g.guild.json')).deleteSync();
+      }
     }
   }
 
